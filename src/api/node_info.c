gqqnbig/slurm

WIP: Transition reboot queued/pending node state to reboot issued state
Bug 10435

src/api/node_info.c
@@ -367,7 +367,8 @@ char *slurm_sprint_node_table(node_info_t *node_ptr, int one_liner)
 
 	/****** Line ******/
 	if ((node_ptr->next_state != NO_VAL) &&
-	    (my_state & NODE_STATE_REBOOT)) {
+	    ((my_state & NODE_STATE_REBOOT) ||
+	     (my_state & NODE_STATE_REBOOT))) {
 		xstrfmtcat(out, "NextState=%s",
 			   node_state_string(node_ptr->next_state));
 		xstrcat(out, line_end);

yulingao/slurm20_11

src/api/node_info.c
@@ -367,7 +367,8 @@ char *slurm_sprint_node_table(node_info_t *node_ptr, int one_liner)
 
 	/****** Line ******/
 	if ((node_ptr->next_state != NO_VAL) &&
-	    (my_state & NODE_STATE_REBOOT)) {
+	    ((my_state & NODE_STATE_REBOOT) ||
+	     (my_state & NODE_STATE_REBOOT_ISSUED))) {
 		xstrfmtcat(out, "NextState=%s",
 			   node_state_string(node_ptr->next_state));
 		xstrcat(out, line_end);